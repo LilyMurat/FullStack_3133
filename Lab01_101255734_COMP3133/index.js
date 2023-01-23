37a8aa66b6138508e61de9ce2e17d0cf04787e45
Lale <100545087+LilyMurat@users.noreply.github.com>
2023-01-22 23:38:10 -0500

    Lab01

---
+++ Lab01_101255734_COMP3133/index.js
@@ -0,0 +1,41 @@
+console.log("Week 02 - File Handling")
+
+//import the fs (inbuild) module for read the file 
+//import { readFile } from "fs"
+var fs = require("fs")
+
+//read the csv file
+// async -> console.log("------Start-----") -> wait the text
+fs.readFile("input_countries.csv", (err, data)=>{
+    if(err){
+        console.log(err) //if data is not available, through error
+        return
+    }
+    console.log(data.toString()) //convert the byte(buffer) to string 
+})
+//console.log("------End-----")
+
+//sync -> don't wait for the text 
+
+//r -> read only
+//r+ -> read + write
+//w -> only write
+//w+ -> write + read
+//a -> append
+//ws -> write sync
+//async, r+ mode, fd -> file descriptor
+
+// Open the input_countries.csv file
+var csv = require('csv-parser');
+fs.createReadStream('input_countries.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    // /Filter data of Canada and write data to canada.txt?
+    if (data.country === 'Canada') {
+      fs.appendFileSync('canada.txt', `${data.country},${data.year},${data.population}\n`);
+    }
+    // c.	Filter data of United States and write data to usa.txt
+    if (data.country === 'United States') {
+      fs.appendFileSync('usa.txt', `${data.country},${data.year},${data.population}\n`);
+    }
+  });
